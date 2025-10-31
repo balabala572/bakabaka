bb38443f94622a7bc08a12c497e67b1f7f4b5707
balabala572 <202510306202@cdu.edu.cn>
2025-10-31 20:13:27 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -1,17 +1,26 @@
 //202510306202
 //202510306202@cdu.edu.en
 //龚慧洁
+#include <stdio.h>
 int main()
 {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    if (a + b > c && b + c > a && a + c > b)
+    int i, h, t, u;
+    i = 100;
+    int is_first=1;
+    while (i <= 999)
     {
-        printf("可以组成三角形\n");
+        h = i / 100;
+        t = (i / 10) % 10;
+        u = (i % 10);
+        if (h * h * h + t * t * t + u * u * u == i)
+        {
+            if(is_first=0){
+            printf("%d", i);
+            is_first=0;}
+            else{
+                printf(" %d",i);}
+            i++;
+            return 0;
+        }
     }
-    else 
-    {
-        printf("不能组成三角形\n");
-    }
-    return 0;
 }
