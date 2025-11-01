5350a3ec2f1c5700afeef09f1213d2934f5578b6
balabala572 <202510306202@cdu.edu.cn>
2025-11-01 17:05:42 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -11,13 +11,13 @@ for(int i=0;i<4;i++){
 }
 arr[4]=sum;
 for(int i=0;i<5;i++){
-    if(i=0){
+    if(i==0){
             printf("%d", arr[i]);
     }
         else
         {
             printf(" %d", arr[i]);
-        }
+        } 
 }
         return 0;
-    }
+}    
