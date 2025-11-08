821b6fec1d298bdcd2fb2c42e480eebdfa6506c8
balabala572 <202510306202@cdu.edu.cn>
2025-11-08 11:44:41 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -5,7 +5,7 @@
 int sum(int arr[],int len){
     int sum=0;
     for (int i=0;i<len;i++){
-        sum=+ arr[i];
+        sum += arr[i];
             }
             return sum;
             }
@@ -18,14 +18,14 @@ int pruduct(int arr[],int len){
 }
 int main(){
     int arr[5];
-    printf("请输入5个整数空格分离");
-    for(int i = 0;i<5;i++){
+    printf("请输入5个整数空格分离:");
+    for(int i = 0;i < 5; i++){
     scanf("%d",&arr[i]);
     }
 int sum_result=sum(arr, 5);
 int product_result=product(arr, 5);
-printf("%d\n",sum);
-printf("%d\n", product);
+printf("%d\n",sum_result);
+printf("%d\n", product_result);
 return 0;
 }
 
