7a47547693ecf96682e6e7e4a0d01ebf925c77c4
balabala572 <202510306202@cdu.edu.cn>
2025-11-08 15:49:54 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -2,7 +2,8 @@
 //202510306202@cdu.edu.cn
 //龚慧洁
 #include <stdio.h>
-int calculatesum(int a1,int an,int step){
+int calculatesum(int a1,int an,int step)
+{
     if (step == 0) {
         if( a1 == an) {
             return a1;
@@ -11,24 +12,16 @@ int calculatesum(int a1,int an,int step){
         return -1;
     }
 }      
-    if (((an-a1)*step < 0)||((an-a1)% step!=0))
+    if (((an-a1) * step < 0)||((an-a1)% step!=0))
     {
         return -1;
 }
-int n=(an-a1) / step + 1;
+int n=(an - a1) / step + 1;
 int sum = n* (a1+an) /2;
 return sum;
 }
 int main() {
-    int a1,an,step;
-    int result;
-    scanf("%d %d %d",&a1, &an, &step);
-result=calculatesum(a1,an,step);
-if (result == -1){
-    printf("输入的不是有效等差数列\n");
-} 
-else {
-printf("%d\n", result);
-}
+    int sum=calculatesum(1, 100, 1);
+printf("从1加到100的和为:%d\n",sum);
 return 0;
 }
