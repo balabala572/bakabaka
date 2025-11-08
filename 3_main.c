311e850497dc790e08e885aa10a9cd679c2df640
balabala572 <202510306202@cdu.edu.cn>
2025-11-08 09:59:15 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -9,17 +9,17 @@ int main()
     int temp;
     int flag;
         int i, j;
-    for(i = 0;i < n - 1; i++){
-        scanf("%d",&arr[i]
-  for (int i = 0; i < n - 1; i++)
+    for(i = 0;i < n ; i++){
+        scanf("%d",&arr[i]);
+  for (int i = 0; i < n - 1 ; i++)
     {
         flag = 0;
         for (int j = 0; j < n - i- 1; j++)
         { 
-            if (arr[j] > arr[j+1]){
+            if (arr[j] > arr[j + 1]){
             temp =arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1] =temp;
+            arr[j]=arr[j + 1];
+            arr[j + 1] =temp;
             flag = 1;
         }
         }
