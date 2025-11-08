253649419be702308390035e260125f6e2c93925
balabala572 <202510306202@cdu.edu.cn>
2025-11-08 08:07:36 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -15,15 +15,15 @@ int main()
         { 
             if (arr[j] > arr[j+1]){
             temp =arr[j];
-            arr[j]=arr[j+i];
-            arr[j+i] =temp;
+            arr[j]=arr[j+1];
+            arr[j+1] =temp;
             flag = 1;
         }
         }
         if (flag==0) break;
     }
     for (int i = 0;i < n; i++){
-        printf ("%d",arr[i]);
+        printf ("%d ",arr[i]);
     }
     return 0;
 }
