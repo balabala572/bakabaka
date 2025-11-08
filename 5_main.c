a4d4ae13b6d88f49587b17c2f237a264254bbb9c
balabala572 <202510306202@cdu.edu.cn>
2025-11-08 10:59:21 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -7,10 +7,13 @@ int calculatesum(int a1,int an,int step){
         if( a1 == an) {
             return a1;
         }
-        else{
+        else {
         return -1;
     }
 }      
+    if ((an-a1))*step < 0) ||((an-a1)% step!=0)){
+        return -1;
+}
 int n=(an-a1) / step + 1;
 int sum = n* (a1+an) /2;
 return sum;
@@ -18,13 +21,13 @@ return sum;
 int main() {
     int a1,an,step;
     int result;
-    scanf("%d %d %d",&a1,&an,&step);
+    scanf("%d %d %d",&a1, &an, &step);
 result=calculatesum(a1,an,step);
-if (result==-1){
+if (result == -1){
     printf("输入的不是有效等差数列\n");
 }
 else {
-printf("%d/n",result);
+printf("%d\n", result);
 }
 return 0;
 }
