da0f917f8cc9afcbe6ff333443a0f5ce3e8706dd
balabala572 <202510306202@cdu.edu.cn>
2025-11-20 23:39:01 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -15,17 +15,19 @@ int main()
     int arr[5];
     int *p = arr;
     int len = 5;
-    printf("请输入5个整数:"\n);
+    int i;
+    printf("请输入5个整数:\n");
     for (int i = 0; i < 5; i++)
     {
         scanf("%d", p + 1);
     }
     two(arr, len);
-    printf("每个元素自增1后的数组:"\n);
-    for (int = 0; i < 5; i++)
+    printf("每个元素自增1后的数组:\n");
+    for (int i = 0; i < 5; i++)
     {
-        printf("%d", *(p + 1);)
+        printf("%d", *(p + 1));
     }
     printf("\n");
     return 0;
 }
+
