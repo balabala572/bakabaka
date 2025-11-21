77eb2e2819c41fedc7e982d0ec6d64b5251bb751
balabala572 <202510306202@cdu.edu.cn>
2025-11-21 23:14:07 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -1,26 +1,40 @@
 //202510306202
 //202510306202@cdu.edu.cn
 //龚慧洁
+
 #include <stdio.h>
-void one(int arr[],int length){
-    int i=0;
-    int j=length-1;
-    while (i<j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-        i++;
-        j--;
+void one(int *arr, int len)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        int *p = arr;
+        for (int j = 0; j < len - 1; j++)
+        {
+            if (*p > *(p + 1))
+            {
+                int temp = *p;
+                *p = *(p + 1);
+                *(p + 1) = temp;
+            }
+            p++;
         }
-}
-int main(){
-    int arr[5];
-    printf("请输入5个整数\n");
-    for(int i=0;i<5;i++){
-        scanf("%d",&arr[i]);
     }
-one(arr,5);
-for(int i=0;i<5;i++){
-printf("%d ",arr[i]);
 }
-}  
+int main()
+{
+    int arr[10];
+    int len = 10;
+    printf("请输入10个整数:");
+    for (int i = 0; i < len; i++)
+    {
+        scanf("%d", arr + i);
+    }
+    one(arr, len);
+    printf("排序结果:");
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+    return 0;
+}
