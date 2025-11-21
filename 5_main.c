ac23f2ae6f54db6203e44ffadb57d204492a0aca
balabala572 <202510306202@cdu.edu.cn>
2025-11-21 22:33:52 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -2,26 +2,29 @@
 //202510306202@cdu.edu.cn
 //龚慧洁
 #include <stdio.h>
-int calculatesum(int a1,int an,int step)
+void two(int *ptr_arr, int len)
 {
-    if (step == 0) {
-        if( a1 == an) {
-            return a1;
-        }
-        else {
-        return -1;
-    }
-}      
-    if (((an-a1) * step < 0)||((an-a1)% step!=0))
+    for (int i = len - 1; i > 0; i--)
     {
-        return -1;
+        *(ptr_arr + i) = *(ptr_arr + i - 1);
+    }
+    *ptr_arr = 0;
 }
-int n=(an - a1) / step + 1;
-int sum = n* (a1+an) /2;
-return sum;
-}
-int main() {
-    int sum=calculatesum(1, 100, 1);
-printf("从1加到100的和为:%d\n",sum);
-return 0;
+int main()
+{
+    int arr[5];
+    int len = 5;
+    printf("请输入5个整数:");
+    for (int i = 0; i < len; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    two(arr, len);
+    printf("移动后的数组:");
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+    return 0;
 }
