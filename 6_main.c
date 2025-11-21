0d8493a72df83a02b5419b643c2d33bdd168644d
balabala572 <202510306202@cdu.edu.cn>
2025-11-21 22:49:32 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -1,31 +1,21 @@
 //202510306202
 //202510306202@cdu.edu.cn
 //龚慧洁
-#include <stdio.h>
-int sum(int arr[],int len){
-    int sum=0;
-    for (int i=0;i<len;i++){
-        sum += arr[i];
-            }
-            return sum;
-            }
-int product(int arr[],int len){
-    int product=1;
-    for(int i = 0;i<len;i++) {
-        product *=arr[i];
-    }
-    return product;
-}
-int main(){
-    int arr[5];
-    printf("请输入5个整数空格分离:");
-    for(int i = 0;i < 5; i++){
-    scanf("%d",&arr[i]);
-    }
-int sum_result=sum(arr, 5);
-int product_result=product(arr, 5);
-printf("%d %d\n",sum_result,product_result);
-return 0;
-}
 
-    
+   #include <stdio.h>
+#include <stdlib.h>
+int main (){
+    int *ptr = (int *)malloc(5 *sizeof(int));
+    printf("请输入5个整数:");
+    for (int i = 0;i < 5;i++){
+        scanf("%d ",ptr + i);
+    }
+    printf("输出:");
+    for(int i = 0;i < 5;i++){
+        printf("%d ",*(ptr + i));
+    }
+    printf("\n");
+    free(ptr);
+    ptr =NULL;
+    return 0;
+} 
